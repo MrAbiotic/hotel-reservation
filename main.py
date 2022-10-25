720c995b706dd690faaa379f72564154e00c716d
Harald Theodor Helland Velde <94077617+MrAbiotic@users.noreply.github.com>
2022-10-25 09:10:04 +0200

    Made som classes

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+class Hotell:
+    def __init__(self, places: int=10) -> None:
+        self.places = places
+
+class Customer:
+    def __init__(self, name: str, guest_count: int) -> None:
+        self.name = name
+        self.guest_count = guest_count
+
 print("Hello World!")
-print("Sup BOYZZ")
