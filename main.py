fa79b596c73caa0e86461a49c99376c6dc3fa31e
Hans Christian Larsen <larsenhanschristian@gmail.com>
2022-10-27 14:55:47 +0200

    deleting

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 class Hotell:
-    def __init__(self, places: int=10, luksusrom: int=2) -> None:
-        self.places = places
+    def __init__(self) -> None:
+        self.etasjer:int = 4
+        self.etasjekapasitet = 12
+
 
 class Customer:
     def __init__(self, name: str, guest_count: int) -> None:
         self.name = name
         self.guest_count = guest_count
-
-print("Hello World!")
-print("test push1234")
-print("Its a meeeeee")
